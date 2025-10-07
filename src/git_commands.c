dbc8a71d997d1043437b1d063b7e4e620dc15fa5
fer-estrada <fernandodestrada@gmail.com>
2025-10-07 11:39:31 +0200

    started merge to main function if in branch

--- src/git_commands.c
+++ src/git_commands.c
@@ -6,17 +6,19 @@ int git_add(char files[]) {
     char add[256];
     sprintf(add, "git add %s", files);
 
-    int call = system(add);
+    int add_files = system(add);
 
-    if (call == 0)
+    if (add_files == 0)
         printf("files added succesfully\n");
     else
-        fprintf(stderr, "failed to add files\n%d", call);
+        fprintf(stderr, "failed to add files\n%d", add_files);
 
     return 0;
 }
 
 int git_commit(char message[]) {
+    char commit[512];
+    
     for (int i = 0; message[i] != '\0'; i++) {
         if (message[i] == '\n') {
             message [i] = '\0';
@@ -24,15 +26,33 @@ int git_commit(char message[]) {
         }
     };
 
-    char commit[512];
     sprintf(commit, "git commit -m \"%s\"", message);
 
-    int call = system(commit);
+    int commit_changes = system(commit);
 
-    if (call == 0)
+    if (commit_changes == 0)
         printf("git committed\n");
     else
-        fprintf(stderr, "failed to perform commit\n%d", call);
+        fprintf(stderr, "failed to perform commit\n%d", commit_changes);
+
+    return 0;
+}
+
+int git_merge_to_main(char branch_name[]) {
+    char return_command[128];
+
+    int checkout_and_pull = system("git checkout main && git pull");
+    if(checkout_and_pull != 0) {
+        fprintf(stderr, "failed to pull from repo/n");
+        return 1;
+    };
+
+    sprintf(return_command, "git checkout \"%s\"", branch_name);
+    int return_to_branch = system(return_command);
+    if(return_to_branch != 0) {
+        fprintf(stderr, "failed to return to branch");
+        return 1;
+    }
 
     return 0;
 }
@@ -40,9 +60,7 @@ int git_commit(char message[]) {
 int git_push() {
     int push = system("git push");
 
-    if (push == 0)
-        printf("pushed to source\n");
-    else
+    if(push != 0)
         fprintf(stderr, "failed to push\n%d", push);
 
     return 0;
--- src/main.c
+++ src/main.c
@@ -1,11 +1,15 @@
 #include "git_commands.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
     char commit_message[256];
     char files_to_add[256];
     char choice;
+    FILE *branch;
+    char output[256];
+    char *branch_name;
 
     printf("which files to add: ");
     fgets(files_to_add, 255, stdin);
@@ -14,6 +18,37 @@ int main() {
     printf("commit message: ");
     fgets(commit_message, 255, stdin);
     git_commit(commit_message);
+
+    branch = popen("git rev-parse --abbrev-ref HEAD", "r");
+    if (branch == NULL)
+        fprintf(stderr, "failed to read branch name\n");
+    if(fgets(output, sizeof(output), branch) != NULL) {
+        output[strcspn(output, "\n")] = 0;
+
+        if(strcmp(output, "HEAD") || strcmp(output, "main") == 0)
+            return 0;
+        else {
+            branch_name = output;
+            printf("you are in a branch [%s], do you want to merge to main ? (y/n): ", branch_name);
+            while(1) {
+                fgets(&choice, 2, stdin);
+
+                if(choice == 'y') {
+                    git_merge_to_main(branch_name);
+                    break;
+                } else if(choice == 'n') {
+                    printf("remained in branch\n");
+                    break;
+                } else {
+                    printf("invalid input try again\n");
+                    printf("you are in a branch [%s], do you want to merge to main ? (y/n): ", branch_name);
+
+                    int c;
+                    while((c = getchar()) != '\n' && c != EOF);
+                }
+            }
+        }
+    }
 
     printf("push to repo ? (y/n): ");
 
